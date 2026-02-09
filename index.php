49e9ca44f4ca45abf3a22b5a7e9dd0694139a207
Felix Roberge <roberge.felix@gmail.com>
2026-02-09 15:21:13 -0500

    add the gitignore

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
 <?php
-// Force errors to show in the browser for debugging
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+// Place this at the VERY top
 error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
-echo "<html><head><title>ROSA PHP Test</title></head><body style='font-family: sans-serif; padding: 20px;'>";
-
-echo "<h1 style='color: #0066cc;'>🚀 PHP is Alive on ROSA!</h1>";
-
-// echo "<ul>";
-// echo "<li><strong>PHP Version:</strong> " . phpversion() . "</li>";
-// echo "<li><strong>Server Date/Time:</strong> " . date('Y-m-d H:i:s') . "</li>";
-// echo "<li><strong>Pod Name:</strong> " . gethostname() . "</li>";
-// echo "<li><strong>User ID:</strong> " . getmyuid() . "</li>";
-// echo "</ul>";
-
-// echo "<hr>";
-// echo "<h3>Server Details:</h3>";
-// echo "<pre>";
-// print_r($_SERVER);
-// echo "</pre>";
-
-echo "</body></html>";
+echo "Testing... If you see this, PHP is working.";
+die(" - Script ended successfully.");
 ?>
