6f9c20e3cee8134faa82fa8c24610c9f7e38927f
Felix Roberge <roberge.felix@gmail.com>
2026-02-09 15:15:18 -0500

    remove the parameter

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ echo "<html><head><title>ROSA PHP Test</title></head><body style='font-family: s
 
 echo "<h1 style='color: #0066cc;'>🚀 PHP is Alive on ROSA!</h1>";
 
-echo "<ul>";
-echo "<li><strong>PHP Version:</strong> " . phpversion() . "</li>";
-echo "<li><strong>Server Date/Time:</strong> " . date('Y-m-d H:i:s') . "</li>";
-echo "<li><strong>Pod Name:</strong> " . gethostname() . "</li>";
-echo "<li><strong>User ID:</strong> " . getmyuid() . "</li>";
-echo "</ul>";
+// echo "<ul>";
+// echo "<li><strong>PHP Version:</strong> " . phpversion() . "</li>";
+// echo "<li><strong>Server Date/Time:</strong> " . date('Y-m-d H:i:s') . "</li>";
+// echo "<li><strong>Pod Name:</strong> " . gethostname() . "</li>";
+// echo "<li><strong>User ID:</strong> " . getmyuid() . "</li>";
+// echo "</ul>";
 
-echo "<hr>";
-echo "<h3>Server Details:</h3>";
-echo "<pre>";
-print_r($_SERVER);
-echo "</pre>";
+// echo "<hr>";
+// echo "<h3>Server Details:</h3>";
+// echo "<pre>";
+// print_r($_SERVER);
+// echo "</pre>";
 
 echo "</body></html>";
 ?>
